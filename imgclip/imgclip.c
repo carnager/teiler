dcd7baf870f792dd8fbed5049209d15357905d76
Rasmus Steinke <rasi@xssn.at>
2014-02-16 12:18:30 +0100

    updated imgclip

--- imgclip/imgclip.c
+++ imgclip/imgclip.c
@@ -12,45 +12,21 @@ on_owner_change(GtkClipboard *cb, GdkEvent *event, gpointer data) {
     }
 }
 
-void 
-on_timeout_expired(gpointer data) {
-    (void) data;
-
-    gtk_main_quit();
-}
-
-int main(int argc, char **argv) {
-    int ret = EXIT_FAILURE;
+int 
+main(int argc, char **argv) {
     GtkClipboard *cb = NULL;
     GdkPixbuf *image = NULL;
     GError *e = NULL;
-    GOptionContext *opt_ctx = NULL;
-    char *help = NULL;
-    static int timeout;
-    static const GOptionEntry entries[] = {
-        { "timeout", 't', 0, G_OPTION_ARG_INT, &timeout, "Timeout", "secs" }, 
-        { NULL, 0, 0, 0, NULL, NULL, NULL }
-    };
-
-    opt_ctx = g_option_context_new("<file>");
-
-    g_option_context_add_main_entries(opt_ctx, entries, NULL);
-    if (!g_option_context_parse(opt_ctx, &argc, &argv, &e)) {
-        fprintf(stderr, "%s\n", e->message);
-        goto error_out;
-    }
 
     if (argc == 1 || !g_file_test(argv[1], G_FILE_TEST_EXISTS)) {
-        help = g_option_context_get_help(opt_ctx, TRUE, NULL);
-        fprintf(stderr, "%s", help);
-        g_free(help);
-        goto error_out;
+        fprintf(stderr, "usage: %s <file>\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
     image = gdk_pixbuf_new_from_file(argv[1], &e);
     if (image == NULL) {
         fprintf(stderr, "%s\n", e->message);
-        goto error_out;
+        return EXIT_FAILURE;
     }
 
     gtk_init(&argc, &argv);
@@ -59,18 +35,9 @@ int main(int argc, char **argv) {
     gtk_clipboard_set_image(cb, image);
     g_signal_connect(G_OBJECT(cb), "owner-change", G_CALLBACK(on_owner_change), NULL);
 
-    if (timeout > 0) {
-        g_timeout_add_seconds(timeout, (GSourceFunc)on_timeout_expired, NULL);
-    }
-
     gtk_main();
 
     g_object_unref(image);
 
-    ret = EXIT_SUCCESS;
-
-error_out: 
-    g_option_context_free(opt_ctx);
-
-    return ret;
+    return EXIT_SUCCESS;
 }
